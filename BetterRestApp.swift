72075752ba1b0ed222452f6f525d1dab7d268efd
KEKSman <bassman7009@gmail.com>
2021-03-18 21:09:13 +0500

    Initial commit

---
+++ BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Евгений колесников on 17.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
